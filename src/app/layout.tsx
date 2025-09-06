db3743b197835efaedfdd6b785fcf9ad813154a8
tilak tiwari <Tilaktiwari789@gmail.com>
2025-09-06 02:42:49 +0000

    Add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'TSIEM',
